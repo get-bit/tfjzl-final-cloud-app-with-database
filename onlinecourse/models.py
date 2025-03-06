e278801035e5686e50f291f01c1b816310be37b3
“get-bit” <maya.chump_0n@icloud.com>
2025-03-06 11:37:26 -0500

    added Submission model

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -98,9 +98,9 @@ class Enrollment(models.Model):
 # One enrollment could have multiple submission
 # One submission could have multiple choices
 # One choice could belong to multiple submissions
-#class Submission(models.Model):
-#    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
-#    choices = models.ManyToManyField(Choice)
+class Submission(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    choices = models.ManyToManyField(Choice)
 
 class Question(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
